2df6db9bf344fb3b49bbd5c33ac3268caaa40f3c
Jacky Meslin <jackymeslin@gmail.com>
2021-04-03 10:43:27 +0200

    test bootstrap

--- app/controllers/megaliths_controller.rb
+++ app/controllers/megaliths_controller.rb
@@ -4,6 +4,28 @@ class MegalithsController < ApplicationController
   # GET /megaliths or /megaliths.json
   def index
     @megaliths = Megalith.all
+
+     # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
+    @markers = @megaliths.geocoded.map do |megalith|
+      if megalith.category == "Menhir"
+        # icon = 'https://res.cloudinary.com/dc9pm7uj8/image/upload/v1587038185/menhirs_zuyjob.png'
+        icon = "menhir.png"
+      elsif megalith.category == "Alignement"
+        # icon = 'https://res.cloudinary.com/dc9pm7uj8/image/upload/v1587038185/menhirs_zuyjob.png'
+        icon = "alignement.png"
+      elsif megalith.category == "Dolmen"
+        # icon = 'https://res.cloudinary.com/dc9pm7uj8/image/upload/v1587038186/dolmen_pklsdz.png'
+        icon = "dolmen.png"
+      end
+      {
+        lat: megalith.latitude,
+        lng: megalith.longitude,
+        # infoWindow: render_to_string(partial: "info_window", locals: { megalith: megalith }),
+        image_url: helpers.asset_url(icon)
+
+      }
+
+    end
   end
 
   # GET /megaliths/1 or /megaliths/1.json
--- app/models/megalith.rb
+++ app/models/megalith.rb
@@ -3,6 +3,15 @@ class Megalith < ApplicationRecord
 
     has_one_attached :photo
 
+    geocoded_by :address
+    after_validation :geocode, if: :will_save_change_to_address?
+
+  # include PgSearch::Model
+  #   pg_search_scope :search_by_name_and_city,
+  #     against: [ :name, :city, :latitude, :longitude],
+  #     using: {
+  #       tsearch: { prefix: true }
+  #   }
 
 
 end
